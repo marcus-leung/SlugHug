2aa9473777ec6428b6f9cce72305fa837a630fe3
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2024-01-20 12:48:59 -0800

    Add Slug grid and pop-up message

--- slughug/src/components/Grid.jsx
+++ slughug/src/components/Grid.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import Slug from './Slug'
+import React, { useState } from "react";
+import Slug from "./Slug";
+import Read from "./Read";
 
 const Grid = () => {
-  return (
-    <div className='pt-2 w-5/6 h-3/4 mt-40 justify-center items-center '>
-        <div className='grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4 px-5 py-5 bg-green-500'>
-            {Array(50).fill(<Slug />)}
-        </div>
-    </div>
-  )
-}
+  const [messageContent, setMessageContent] = useState("");
+  const [isMessageOpen, setIsMessageOpen] = useState(false);
 
-export default Grid
+  const handleSlugClick = (text) => {
+    if (!isMessageOpen) {
+      setMessageContent(text);
+      setIsMessageOpen(true);
+      console.log("CLICKEDZ");
+    }
+  };
+
+  return (
+    <div className="pt-2 w-5/6 h-3/4 mt-40 justify-center items-center ">
+      <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4 px-5 py-5 bg-green-500">
+        {Array(50).fill(
+          <div onClick={() => handleSlugClick("bruh")}>
+            <Slug />
+          </div>
+        )}
+      </div>
+
+      {isMessageOpen && (
+        <Read message="This is a message" onClick={() => setIsMessageOpen(false)}/>
+      )}
+    </div>
+  );
+};
+
+export default Grid;
--- slughug/src/components/Read.jsx
+++ slughug/src/components/Read.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-const Read = () => {
+const Read = ({ message, onClick }) => {
+
   return (
-    <div>Read</div>
-  )
-}
+    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+      <div className="bg-yellow-500 p-8 border-4 border-amber-800 rounded-lg shadow-md max-w-md w-full">
+        <p>{message}</p>
+        <button
+          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={onClick}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default Read
+export default Read;
