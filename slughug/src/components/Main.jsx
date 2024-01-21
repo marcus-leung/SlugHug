12f4b1b90c0bb35b849fe3ce3443c59e1f2523af
Marcus Leung <82466170+marcus-leung@users.noreply.github.com>
2024-01-20 20:54:09 -0800

    Add Therapist Slug

--- slughug/src/components/Main.jsx
+++ slughug/src/components/Main.jsx
@@ -23,7 +23,7 @@ const Main = () => {
         </button>
       </div>
       {isAccountOpen && (
-        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
           <Account onClick={() => setIsAccountOpen(false)} />
         </div>
       )}
--- slughug/src/components/Slug.jsx
+++ slughug/src/components/Slug.jsx
@@ -6,11 +6,17 @@ import Slug4 from "../assets/Slugs/GridSlug4.png";
 import Slug5 from "../assets/Slugs/GridSlug5.png";
 import Slug6 from "../assets/Slugs/GridSlug6.png";
 import Slug7 from "../assets/Slugs/GridSlug7.png";
+import TherapistSlug1 from "../assets/Slugs/TherapistSlug1.png";
 
-const Slug = () => {
+const Slug = ({ type }) => {
   const slug_arr = [Slug1, Slug2, Slug3, Slug4, Slug5, Slug6, Slug7];
 
   const rand_index = Math.floor(Math.random() * slug_arr.length);
+
+  const isProf = false;
+  if (type == "professional") {
+    isProf = true;
+  }
 
   const [jump, setJump] = useState(false);
   useEffect(() => {
@@ -29,7 +35,11 @@ const Slug = () => {
         jump ? "jump" : ""
       }`}
     >
-      <img src={slug_arr[rand_index]} alt="slug" draggable="false" />
+      {isProf ? (
+        <img src={TherapistSlug1} alt="slug" draggable="false" />
+      ) : (
+        <img src={slug_arr[rand_index]} alt="slug" draggable="false" />
+      )}
     </div>
   );
 };
