8c1e77427200ec8368f9c808d8e8ca50e5c61528
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-01-20 20:56:13 -0800

    auth0 update

--- slughug/src/components/AuthenticationSection.jsx
+++ slughug/src/components/AuthenticationSection.jsx
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import LoginButton from "./LoginButton";
 import Logout from "./Logout";
+import LoginButton from "./LoginButton";
+
+async function createUser(data) {
+    try {
+        const response = await fetch('http://localhost:5000/users/add', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+
+        const result = await response.json();
+        console.log("Success:", result)
+    } catch (err) {
+        console.error(err)
+    }
+}
 
 const AuthenticationSection = () => {
     const { user, isAuthenticated } = useAuth0();
 
+    useEffect(() => {
+        console.log("useEffect triggered", { isAuthenticated, user });
+        if(isAuthenticated){
+            console.log("user", user.name)
+            createUser(user.name)
+        }
+
+    }, [user, isAuthenticated]);
+
+
     if (isAuthenticated) {
-        return <div>Welcome, {user.name}, <Logout/></div>;
+        return <div>Welcome, {user.name} <Logout /></div>;
     } else {
-        // return <LoginButton />;
+        return <LoginButton />;
     }
 };
 
