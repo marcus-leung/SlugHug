5f43bbb29080e7c9afd3e4c23ac778260eec4dfb
Aniket Tyagi <freeshavaacadooo@gmail.com>
2024-01-21 03:45:15 -0800

    Updating write, making it like reply

--- slughug/src/components/Write.jsx
+++ slughug/src/components/Write.jsx
@@ -2,19 +2,26 @@ import React from "react";
 import SlugWindow from "../assets/SlugWindow.png";
 
 const Write = ({ onClick }) => {
+
+  const handleWrite= (event) => {
+    event.preventDefault();
+    console.log("THIS IS FROM HANDLE FUNCTION!!!!!")
+    console.log(event.target.Subject.value)
+    console.log(event.target.Message.value)
+    //send();
+  };
+  
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
       <div className="w-full h-5/6 relative">
-        <div className="read-container">
-          <img
+        <img
           src={SlugWindow}
           alt="Slug Window"
           className="block mx-auto"
           draggable="false"
         />
-        </div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-md mt-20 text-black">
-          <form>
+          <form onSubmit={handleWrite}>
             <label
               htmlFor="head"
               className="font-bold block text-center mb-2 text-xl"
@@ -25,15 +32,15 @@ const Write = ({ onClick }) => {
               rows="1"
               cols="90"
               type="text"
-              id="message"
-              name="Message"
+              id="subject"
+              name="Subject"
               className="block  mx-auto p-3 rounded-lg  bg-yellow-100"
             ></textarea>
             <label
               htmlFor="message"
               className="font-bold block text-center mb-2 text-xl pt-5"
             >
-              Message
+              Reply Message
             </label>
             <textarea
               rows="8"
