84c4111d0eb279bf7ac1862360cc66432e2dd812
PangYuanbo <147596230+PangYuanbo@users.noreply.github.com>
2024-01-20 22:24:09 -0800

    find-by-name

--- server/routes/user.js
+++ server/routes/user.js
@@ -25,6 +25,23 @@ recordRoutes.route("/users").get(async function (req, response) {
     });
 
 });
+// GET route to find user by userName
+recordRoutes.route('/users/find-by-name').get(function (req, res) {
+    let db_connect = dbo.getDb();
+    let userNameQuery = req.query.userName; //
+
+    db_connect.collection("users").findOne({ userName: userNameQuery }, function (err, result) {
+        if (err) {
+            res.status(500).send('Error occurred while trying to retrieve user');
+        } else {
+            if (result) {
+                res.status(200).json(result);
+            } else {
+                res.status(404).send('User not found');
+            }
+        }
+    });
+});
  
 // This section will help you create a new user.
 recordRoutes.route("/users/add").post(function (req, response) {
