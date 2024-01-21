ebae7091e5b6ad455b58ad6a5cbaa5fa0ccbc826
Aniket Tyagi <freeshavaacadooo@gmail.com>
2024-01-21 04:43:18 -0800

    Adding sentiment analysis API

--- slughug/src/components/Grid.jsx
+++ slughug/src/components/Grid.jsx
@@ -19,6 +19,45 @@ const Grid = () => {
   // adjust let -> setState
   // instead of db = -> setDBContent(data)
 
+  // Meaningcloud Sentiment Analysis API
+  function analyzeSentiment(event) { 
+    event.preventDefault(); 
+    const apiKey = "e1c2247e1f181a436994dbbc262adffb"; 
+    const text = document.getElementById("content").value; 
+  
+    const url = `https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&txt=${encodeURIComponent( text )}&lang=en`; 
+  
+    fetch(url) 
+    .then((response) => { 
+      if (response.ok) { 
+        return response.json(); 
+      } else { 
+        throw new Error("Request failed"); 
+      } 
+    }) 
+    .then((data) => { 
+      const sentiment = data.score_tag; 
+      // console.log(`Text: ${text}`); 
+      // console.log(`Sentiment: ${sentiment}`); 
+      if (sentiment == "P+") { 
+        document.getElementById("result").innerHTML = "Strong Positive"; 
+      } if (sentiment == "P") { 
+        document.getElementById("result").innerHTML = "Positive"; 
+      } if (sentiment == "NEU") { 
+        document.getElementById("result").innerHTML = "Neutral"; 
+      } if (sentiment == "N") { 
+        document.getElementById("result").innerHTML = "Negative"; 
+      } if (sentiment == "N+") { 
+        document.getElementById("result").innerHTML = "Strong Negative"; 
+      } if (sentiment == "NONE") { 
+        document.getElementById("result").innerHTML = "Without Polarity"; 
+      } 
+    }) 
+    .catch((error) => { 
+      console.error("Error:", error); 
+    }); 
+} 
+
   // HTTP Server Fetch Request definition
   async function getMessages() {
     try {
