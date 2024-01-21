84a99cc018a7bf3c112b84003eefcda6b9749506
Konan Le <kkhoadangle@gmail.com>
2024-01-20 19:58:10 -0800

    made slugs bounce

--- slughug/src/components/Slug.jsx
+++ slughug/src/components/Slug.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Slug1 from '../assets/Slugs/GridSlug1.png';
 import Slug2 from '../assets/Slugs/GridSlug2.png';
 import Slug3 from '../assets/Slugs/GridSlug3.png';
 
 const Slug = () => {
   const slug_arr = [Slug1, Slug2, Slug3];
+  const rand_index = Math.floor(Math.random() * slug_arr.length);
+  const [jump, setJump] = useState(false);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setJump(true);
 
-  const rand_index = Math.floor(Math.random() * slug_arr.length)
-  
+      // Reset jump after animation duration (500ms)
+      setTimeout(() => setJump(false), 500);
+    }, Math.random() * 5000 + 3000); // Random interval between 1s and 4s
+
+    return () => clearInterval(timer);
+  }, []);
   return (
-    <div className='cursor-pointer aspect-square w-[170px] h-[170px] justify-center items-center'>
+    <div className={`cursor-pointer aspect-square w-[170px] h-[170px] justify-center items-center ${jump ? 'jump' : ''}`}>
         <img src={slug_arr[rand_index]} alt="slug" />
     </div>
   )
